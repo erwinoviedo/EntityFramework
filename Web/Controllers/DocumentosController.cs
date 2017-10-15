2fee8fe769ba6dc57fead1d2658020eb1148aba5
Erwin Oviedo <erwin.oviedo@gmail.com>
2017-10-15 19:05:27 -0400

    Clase Sabado

---
+++ Web/Controllers/DocumentosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+namespace Web.Controllers
+{
+    public class DocumentosController : Controller
+    {
+        // GET: Documentos
+        public ActionResult Index()
+        {
+            var appContext = new ApplicationContext();
+            List<Entidades.Documento> listaDocumentos = appContext.Documentos.ToList();
+            return View(listaDocumentos);
+        }
+    }
+}
