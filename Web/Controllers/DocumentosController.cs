9926cbe0b0fcc7f47ac0e62a30d460d78f2a27a2
Erwin Oviedo <erwin.oviedo@gmail.com>
2017-10-19 21:24:09 -0400

    Clase Martes

--- Web/Controllers/DocumentosController.cs
+++ Web/Controllers/DocumentosController.cs
@@ -15,5 +15,37 @@ namespace Web.Controllers
             List<Entidades.Documento> listaDocumentos = appContext.Documentos.ToList();
             return View(listaDocumentos);
         }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(Models.DocumentoViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //Escribir codigo para guardar a la BD.
+                var appContext = new ApplicationContext();
+                //Pasar los datos del modelo a la entidad Documento
+                var nuevoDocumento = new Entidades.Documento();
+                nuevoDocumento.CITE = model.CITE;
+                nuevoDocumento.Referencia = model.Referencia;
+                //Valores por defecto (para que permita el guardado)
+                nuevoDocumento.DestinatarioId = 1;
+                nuevoDocumento.EmisorId = 2;
+                nuevoDocumento.FechaCreacion = DateTime.Today;
+                nuevoDocumento.FechaRecepcion = DateTime.Today;
+                nuevoDocumento.RedirigidoFecha = DateTime.Today;
+                nuevoDocumento.RedirigidoId = 3;
+                
+                appContext.Documentos.Add(nuevoDocumento);
+                appContext.SaveChanges();
+                //Redireccionar a la vista principal
+                return RedirectToAction("Index", "Documentos");
+            }
+            return View(model);
+        }
     }
 }
---
+++ Web/Models/DocumentoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class DocumentoViewModel
+    {
+        [Required(ErrorMessage = "El codigo CITE es requerido.")]
+        [StringLength(5, ErrorMessage = "El codigo CITE no puede contener mas de 5 caracteres")]
+        public string CITE { get; set; }
+
+        public string Referencia { get; set; }
+    }
+}
